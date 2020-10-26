f344fd61d219f3cf84b49aba9490ae60d9bd3b69
Krileks91 <pandorian91@gmail.com>
2020-10-26 20:39:44 +0100

    dovrsen primer

--- app/src/main/java/com/example/termin22/MyIntenrService.java
+++ app/src/main/java/com/example/termin22/MyIntenrService.java
@@ -2,6 +2,7 @@ package com.example.termin22;
 
 import android.app.IntentService;
 import android.app.Notification;
+import android.app.NotificationManager;
 import android.content.Intent;
 import android.os.Build;
 import android.os.SystemClock;
@@ -32,22 +33,34 @@ public class MyIntenrService extends IntentService {
     @Override
     protected void onHandleIntent(@Nullable Intent intent) {
         String msg = intent.getStringExtra(MSG_TAG);
-        for (int i = 0; i < 10 ; i++) {
+        for (int i = 0; i < 10; i++) {
             SystemClock.sleep(1000);
 
-            Log.e(TAG,msg + " : " + i);
+            Log.e(TAG, msg + " : " + i);
 
         }
     }
 
-    private void showNotifIfNeeded(){
-        if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.O){
+    private void showNotifIfNeeded() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
             Notification notification = new NotificationCompat.Builder(this, MyApp.CHANNEL_ID)
                     .setContentTitle("Our Intent Service")
                     .setContentText("Working smart, not hard")
                     .setSmallIcon(R.drawable.ic_baseline_confirmation_number_24)
                     .build();
             startForeground(NOTIF_ID, notification);
+        }
+    }
+
+    private void updateCountDownNotification(int seconds, String string) {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            Notification notification = new NotificationCompat.Builder(this, MyApp.CHANNEL_ID)
+                    .setContentTitle("Nova notifikacija")
+                    .setContentText(string + " - " + seconds)
+                    .setSmallIcon(R.drawable.ic_baseline_confirmation_number_24)
+                    .build();
+            NotificationManager notificationManager = (NotificationManager) getSystemService(NOTIFICATION_SERVICE);
+            notificationManager.notify(NOTIF_ID, notification);
         }
     }
 }
